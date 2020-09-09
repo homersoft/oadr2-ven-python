b9f75bce605e0338ac4166578d0b0406d08f2a1e
Rafał Gajda <rafal.gajda@silvair.com>
2020-09-09 17:01:28 +0200

    Use requests for polling VTN

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'oadr2-ven',
-    version = '0.4',
+    version = '0.5',
     description = 'OpenADR 2.0a VEN for Python',
     author = 'EnerNOC Advanced Technology',
     author_email = 'tnichols@enernoc.com',
     url = 'http://open.enernoc.com',
     packages = find_packages('.', exclude=['*.tests']),
-    install_requires = ['lxml', 'sleekxmpp', 'dnspython', 'python-dateutil'],
+    install_requires = ['lxml', 'sleekxmpp', 'dnspython', 'python-dateutil', 'requests'],
     tests_require = ['freezegun'],
     zip_safe = False,
 )
